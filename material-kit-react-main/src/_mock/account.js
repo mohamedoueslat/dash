99b3d24b498e9e68738f7b05cdd715cd343daca0
Mohame0136 <mohame0136@example.com>
2023-01-08 12:40:10 +0100

    first commit

---
+++ material-kit-react-main/src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'mohamed',
+  email: 'mohamed@risefortheworld.com',
+  photoURL: '/assets/images/avatars/téléchargement.png',
+}
+
+export default account;
